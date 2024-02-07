2ebdcc69006afbace3494061137ca4c121c2208e
Sohail <51455139+sohail-coder@users.noreply.github.com>
2024-02-06 23:39:26 -0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar(){
 
 def buildImageAndPush(){
     echo "building the docker image"
-    withCredentials([usernamePassword(credentialsId:'github',usernameVariable:'USER',passwordVariable:'PASS')]){
+    withCredentials([usernamePassword(credentialsId:'dockerhub',usernameVariable:'USER',passwordVariable:'PASS')]){
         sh "docker build sohail233/devops:jma-dockerHub-1.0"
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push sohail233/devops:jma-dockerHub-1.0"
