2d7bec50d47af6c146cf0b85420994e82eb10580
sohail shaik <shaiksohail2332000@gmail.com>
2024-02-10 11:06:43 -0800

    test check 2

--- script.groovy
+++ script.groovy
@@ -23,7 +23,7 @@ def commitToGit(){
         sh "git remote set-url origin https://$USER:$PASS@github.com/sohail-coder/devops.git"
         sh 'git add .'
         sh 'git commit -m "ci-skip"'
-        sh 'git push origin HEAD:devOps' 
+        sh 'git push HEAD:devOps' 
     }
 }
 return this
